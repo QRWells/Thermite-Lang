9651ebb92f55534aa6fad69a7aa9b7fa56751290
QRWells <qirui.wang@moegi.waseda.jp>
2022-02-05 14:16:47 +0900

    Rewrite hard-coded file read

--- src/main.cc
+++ src/main.cc
@@ -11,41 +11,25 @@
  */
 
 #include "parser/Lexer.h"
-#include "parser/TokenClass.h"
+#include "parser/Token.h"
 
+#include <fmt/core.h>
 #include <fstream>
 #include <iostream>
 #include <string>
 
 auto main(int argc, char *const argv[]) -> int {
+  if (argc < 2)
+    return -1;
   std::ifstream Code;
-  Code.open("/mnt/c/Users/wqrmc/Documents/GitHub/Thermite-Lang/test/test.tm");
+  Code.open(*(argv + 1));
+  if (!Code.is_open())
+    return -1;
   thermite::Lexer L(Code);
-  thermite::TokenClass T(thermite::TokenClass::Eof);
-  double Val = 0;
-  std::string S;
-  char C = 0;
-  while (true) {
-    T = L.getToken();
-    if (T == thermite::TokenClass::Eof)
-      break;
-    std::cout << thermite::tokenToCstr(T) << " : ";
-    if (T == thermite::TokenClass::Number) {
-      Val = L.getNumber();
-      std::cout << Val << std::endl;
-      continue;
-    }
-    if (T == thermite::TokenClass::Unknown) {
-      C = L.getUnknown();
-      std::cout << C << std::endl;
-      continue;
-    }
-    if (T == thermite::TokenClass::BinOperator) {
-      std::cout << static_cast<char>(L.getBinOperator()) << std::endl;
-      continue;
-    }
-    S = L.getIdentifier();
-    std::cout << S << std::endl;
+  auto Token = L.getToken();
+  while (Token.getClass() != thermite::TokenClass::Eof) {
+    fmt::print("{}\n", Token.toString());
+    Token = L.getToken();
   }
   Code.close();
   return 0;
